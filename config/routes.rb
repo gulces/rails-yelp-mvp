83daa37e91eb0bb7b788743bdac1d6819d76695a
Gulce Sakallioglu <gulcesakallioglu@gmail.com>
2021-11-10 14:48:05 +0000

    restaurant and review controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get 'restaurants/:id'
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
 end
